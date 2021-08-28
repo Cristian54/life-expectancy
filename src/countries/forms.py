b3e51ae3555cb642bc3e1f3d64ec2e9905ed808e
cgalvan9 <cgalvan9@ucmerced.edu>
2021-08-28 07:43:36 -0700

    styled buttons on country page

--- src/countries/forms.py
+++ src/countries/forms.py
@@ -5,6 +5,11 @@ from .models import CountriesData
 class CountryForm(forms.Form):
     countries = CountriesData.objects.values('Country').distinct()
     clist = [(c['Country'], c['Country']) for c in countries]
+    clist.insert(0, ('', 'Select a country'))
     ctuple = tuple(clist)
-    
-    country_list = forms.ChoiceField(choices=ctuple, widget=forms.Select, label="Select a country")
+
+    country_list = forms.ChoiceField(choices=ctuple, label="", required=True,
+                                     widget=forms.Select(attrs={
+                                            'style': 'max-width: 300px;',
+                                            'class':'form-control'
+                                    }))
